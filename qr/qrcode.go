6ad87b237aa41e7f9e3270de3d013047c6349235
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-11-10 14:41:56 +0100

    Add wrapper around QR library

---
+++ qr/qrcode.go
@@ -0,0 +1,22 @@
+package qr
+
+import (
+	"fmt"
+	"image"
+
+	"github.com/skip2/go-qrcode"
+)
+
+// Encode encodes data into the image with QR code.
+func Encode(data string, size int) (image.Image, error) {
+	qr, err := qrcode.New(data, qrcode.Medium)
+	if err != nil {
+		return nil, fmt.Errorf("encode QR: %v", err)
+	}
+	return qr.Image(size), nil
+}
+
+// Decode an image with QR code.
+func Decode(img image.Image) (string, error) {
+	return "", fmt.Errorf("not implemented")
+}
