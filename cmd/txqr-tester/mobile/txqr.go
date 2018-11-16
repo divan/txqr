07f96aaab7603efbd3d967e627165f6b8f13966e
Ivan Danyliuk <ivan.daniluk@gmail.com>
2018-11-16 20:39:08 +0100

    Add WebSocket client to mobile part

--- cmd/txqr-tester/mobile/txqr.go
+++ cmd/txqr-tester/mobile/txqr.go
@@ -1,21 +1,63 @@
 package txqrtester
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/divan/txqr/cmd/txqr-tester/ws"
+	"github.com/gorilla/websocket"
+)
+
 // Connector represents a connection to the txqr-tester app
 // via websockets.
 type Connector struct {
+	conn *websocket.Conn
 }
 
 // Connect attempts to establish connection to the WS server.
 func (c *Connector) Connect(address string) error {
-	return nil
+	conn, _, err := websocket.DefaultDialer.Dial(address, nil)
+	if err != nil {
+		return fmt.Errorf("dial: %v", err)
+	}
+	c.conn = conn
+	return c.connect()
 }
 
 // StartNext notifies txqr-tester that app is ready to scan next animated QR.
 func (c *Connector) StartNext() error {
-	return nil
+	return c.sendCommand(ws.UIRequest{
+		Cmd: ws.CmdStartNext,
+	})
 }
 
 // SendResult sends scanning result in milliseconds.
-func (c *Connector) SendResult(duration int) error {
-	return nil
+func (c *Connector) SendResult(duration int64) error {
+	return c.sendCommand(ws.UIRequest{
+		Cmd:      ws.CmdResult,
+		Duration: duration,
+	})
+}
+
+// Close closes underlying WebSocket connection.
+func (c *Connector) Close() error {
+	if c.conn == nil {
+		return nil
+	}
+	return c.conn.Close()
+}
+
+func (c *Connector) connect() error {
+	return c.sendCommand(ws.UIRequest{
+		Cmd:    ws.CmdConnect,
+		Device: "N/A",
+	})
+}
+
+func (c *Connector) sendCommand(req ws.UIRequest) error {
+	data, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("json marshal: %v", err)
+	}
+	return c.conn.WriteMessage(websocket.TextMessage, data)
 }
